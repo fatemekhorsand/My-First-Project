38feb1e23ca4233f13f0d83d3d95771573fef4ea
Fatemeh-Khorsand <fatemekhorsand1100@gmail.com>
2025-07-23 12:33:18 +0330

    Create firstCode.cs

---
+++ firstCode.cs
@@ -0,0 +1,9 @@
+using system;
+class program
+{
+  static void main(string[] args)
+  {
+    Console.WriteLine("Hellooooo :)))  It's my first project!");
+  }
+}
+// اضافه کردن یک قطعه کد ساده به گیت هاب برای تمرین
